7e1e73deef8b8ace9711e93bf8352582f6308bd7
Jean-Baptiste Carré <jbc@0x1d.org>
2012-06-11 15:47:27 +0200

    added release code: if a dep is required by an other project, the project is released and linked

--- publish.sh
+++ publish.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
+function import_dependency {
+    dep_dir="$1"
+    if [ -d $dep_dir ]; then
+	cd $dep_dir
+	bash publish.sh $ppath/deps
+    else
+	echo "WARNING: $dep_dir can't be found!"
+    fi
+}
+
 git describe --always --tags > VERSION
 
 ppath=$( pwd )
 foldername=${ppath##*/}
 releasename=$foldername-$( cat VERSION )
 
+### dependencies importation  ###
+
+# Add here, if necessary
+# USAGE: import_dependency "rel-or-abs-path-to-the-project"
+
+### Release ###
+
+cd $ppath
 
 if [ ! -d releases/$releasename ]; then
     mkdir -p releases/$releasename
 fi
 
 rsync -a --exclude 'releases' $ppath/ releases/$releasename
+
+### If a release is required as a dependency ###
+
+if [ $# -eq 1 ]; then
+  cp -R releases/$releasename "$1"
+  ln -s $releasename "$1"/$foldername
+fi
