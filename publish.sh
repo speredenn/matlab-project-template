daee058f2d920a504b329c8dd20b7343ae34ba2d
Jean-Baptiste Carré <jbc@0x1d.org>
2012-06-11 14:13:00 +0200

    git-sha + rsync in publish.sh defaults

--- publish.sh
+++ publish.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+git describe --always --tags > VERSION
+
+ppath=$( pwd )
+foldername=${ppath##*/}
+releasename=$foldername-$( cat VERSION )
+
+
+if [ ! -d releases/$releasename ]; then
+    mkdir -p releases/$releasename
+fi
+
+rsync -a --exclude 'releases' $ppath/ releases/$releasename
